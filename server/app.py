1e21528ccf857d5cc76bac4eebd473ef8fe73634
Edmund <edmundgachanja@gmail.com>
2025-03-30 14:50:35 +0300

    lab

--- server/app.py
+++ server/app.py
@@ -47,6 +47,25 @@ class PlantByID(Resource):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
 
+    def patch(self, id):
+        plant = Plant.query.filter(Plant.id==id).first()
+        for attr in request.json:
+            setattr(plant, attr, request.json[attr])
+        
+        db.session.add(plant)
+        db.session.commit()
+
+        response = plant.to_dict()
+        return make_response(response, 200)
+    
+    def delete(self, id):
+        plant = Plant.query.filter(Plant.id==id).first()
+
+        db.session.delete(plant)
+        db.session.commit()
+
+        return make_response("", 204)
+
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
